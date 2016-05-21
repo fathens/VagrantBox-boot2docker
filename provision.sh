a00ea8e58fec4b9a3bc2df91f714fa99d40df6e2
Kunio Sawatani <devel@fathens.org>
2016-05-21 19:58:02 +0900

    apt list の修正
    http://askubuntu.com/questions/265007/ubuntu-12-04-why-does-apt-get-update-fail

--- provision.sh
+++ provision.sh
@@ -10,15 +10,16 @@ ls -la ~/.ssh
 # Add to sudoers
 echo 'vagrant' | sudo -S sh -c "echo 'vagrant ALL=(ALL) NOPASSWD: ALL' > /etc/sudoers.d/vagrant && chmod 0440 /etc/sudoers.d/vagrant"
 
-# Instal Docker
+# Fix apt lists
+sudo rm -rfv /var/lib/apt/lists/*
 sudo apt-get update -y
+
+# Instal Docker
 sudo apt-get install -y apt-transport-https ca-certificates
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 sudo su -c "echo 'deb https://apt.dockerproject.org/repo ubuntu-precise main' >  /etc/apt/sources.list.d/docker.list"
 sudo apt-get update -y
-apt-cache policy docker-engine
 
-# sudo apt-get install -y linux-image-generic-lts-trusty
 sudo apt-get install -y docker-engine
 sudo service docker start
 sudo groupadd docker
