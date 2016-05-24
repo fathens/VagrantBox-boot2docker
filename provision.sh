89b692506608fb40b103d12c6878891c92c10e75
Kunio Sawatani <devel@fathens.org>
2016-05-24 10:02:36 +0900

    VagrantBox ubuntu/trusty64 をベースにするよう変更

--- provision.sh
+++ provision.sh
@@ -1,17 +1,5 @@
 set -x
 
-# ssh authorized_keys
-mkdir ~/.ssh
-wget -qO- https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub > ~/.ssh/authorized_keys
-chmod 700 ~/.ssh
-chmod 600 ~/.ssh/authorized_keys
-ls -la ~/.ssh
-
-# Add to sudoers
-echo 'vagrant' | sudo -S sh -c "echo 'vagrant ALL=(ALL) NOPASSWD: ALL' > /etc/sudoers.d/vagrant && chmod 0440 /etc/sudoers.d/vagrant"
-
-# Fix apt lists
-sudo rm -rfv /var/lib/apt/lists/*
 sudo apt-get update -y
 
 # Instal Docker
